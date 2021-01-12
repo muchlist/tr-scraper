ccfb8aa26bd583123d3e6f6f885c8756bbfb29cc
muchlist <whois.muchlis@gmail.com>
2021-01-13 06:09:38 +0700

    regex

--- level2/regex.py
+++ level2/regex.py
@@ -19,3 +19,23 @@ bbbbb :
 (d|e) :
     d atau e
 """
+
+import re
+from urllib.request import urlopen
+
+from bs4 import BeautifulSoup
+
+html = urlopen('http://www.pythonscraping.com/pages/page3.html')
+bs = BeautifulSoup(html, 'html.parser')
+images = bs.find_all('img', {'src': re.compile('\.\./img/gifts/img.*\.jpg')})
+# images = bs.find_all('img', {'src': re.compile('\.\.\/img\/gifts/img.*\.jpg')})
+for image in images:
+    print(image['src'])
+
+"""
+../img/gifts/img1.jpg
+../img/gifts/img2.jpg
+../img/gifts/img3.jpg
+../img/gifts/img4.jpg
+../img/gifts/img6.jpg
+"""
